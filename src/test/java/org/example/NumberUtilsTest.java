c1d27a6ec0443657ae210a2e03a51aab12a0e563
Dawood <RDAWOOD1@depaul.edu>
2025-03-18 16:41:21 -0500

    Add Assignment 8 folder

---
+++ src/test/java/org/example/NumberUtilsTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class NumberUtilsTest {
+    /**
+     *
+     * Step 1: understand the requirement, input type and output type
+     *        Requirement: Add two list of integer, index by index, and returns another list
+     *
+     * Step 2 (raw):  Perform partition and boundary analysis on input and output
+     *        Each input: left | right
+     *        Combination of input:
+     *        Output:
+     *  Step 3: Derive potential test cases
+     *
+     */
+
+}
